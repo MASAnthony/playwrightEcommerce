3a32a7f872b32a6064d533af365f4810ee16127a
anthony.m <anthony.m@skill-mine.com>
2026-02-24 19:32:03 +0530

    fully refactored the login flow

--- fixtures/testFixture.ts
+++ fixtures/testFixture.ts
@@ -1,10 +1,12 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pages/auth/LoginPage';
+import { LocationPage } from '../pages/auth/LocationPage';
 import { DashboardPage } from '../pages/dashboard/DashboardPage';
 import { testData } from '../utils/testData';
 
 type MyFixtures = {
     loginPage: LoginPage;
+    locationPage: LocationPage;
     dashboardPage: DashboardPage;
     loggedInPage: Page;
 };
@@ -13,14 +15,16 @@ export const test = base.extend<MyFixtures>({
     loginPage: async ({ page }, use) => {
         await use(new LoginPage(page));
     },
+    locationPage: async ({ page }, use) => {
+        await use(new LocationPage(page));
+    },
     dashboardPage: async ({ page }, use) => {
         await use(new DashboardPage(page));
     },
     loggedInPage: async ({ page }, use) => {
         const loginPage = new LoginPage(page);
         await loginPage.navigate();
-        await loginPage.login(testData.admin.username, testData.admin.password);
-        await loginPage.validateLoginSuccess();
+        await loginPage.completeLogin(testData.user.mobile, testData.user.otp);
         await use(page);
     },
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,17 @@ export default defineConfig({
         timeout: 5000
     },
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 1,
+    retries: process.env.CI ? 2 : 0,
+
+    /* Opt out of parallel tests on CI. Run tests sequentially locally to save system resources. */
+    workers: 1,
+
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    // workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['html'],
@@ -42,7 +46,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.BASE_URL || 'https://dev.example.com',
+        baseURL: process.env.BASE_URL || 'https://staging-qc.sterlingaccuris.com',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -57,14 +61,14 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
 
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
 
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
     ],
 });
